0c6e7df4084ef7510f9ab37ba7e8b34160adf56d
Sanika Chandode <sanu30chandode@gmail.com>
2023-02-11 17:46:09 +0530

    Notifications implemented

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,10 +7,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:unscript_app/app/app.dart';
 import 'package:unscript_app/login/login.dart';
 import 'package:unscript_app/register/layout/verify_otp_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getToken();
+  //push notifications
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
   const orientations = [
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown
@@ -31,6 +49,12 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
+    FirebaseMessaging.instance.getToken().then((newToken) {
+      print("FCM Token: ");
+      print(newToken);
+      // // save device token to server
+      // tokenService.updateTokenOnServer(newToken);
+    });
     switchHome();
     super.initState();
   }
